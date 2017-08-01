917e5bcbbdb4559f275a52814595553d729a63c2
Cem Berk Öngay <cberkongay@gmail.com>
2017-08-02 00:39:58 +0300

    To-Do List App

---
+++ TodoList.Web/Controllers/TodoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace TodoApp.Web.Controllers
+{
+    public class TodoController : Controller
+    {
+        public static IList<Todo> Todos = new List<Todo>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult List()
+        {
+            return JsonSuccess(Todos);
+        }
+        public JsonResult Add(string text)
+        {
+            var newTodo = new Todo() { Text = text};
+            Todos.Add(newTodo);
+            newTodo.Modified = DateTime.Now;
+            return JsonSuccess(newTodo, "Success.");
+        }
+        public JsonResult Update(Guid? id, string text)
+        {
+            var todo = Todos.SingleOrDefault(x => x.Id == id);
+            if (todo == null)
+                return JsonError("Update Error!");
+            todo.Text = text;
+            todo.Modified = DateTime.Now;
+            return JsonSuccess(todo, "Update Succes.");
+        }
+        //public JsonResult Delete(Guid? id)
+        //{
+        //    var todo = Todos.SingleOrDefault(x => x.Id == id);
+        //    if (todo == null)
+        //        return JsonError("Delete Error!");
+        //    Todos.Remove(todo);
+        //    return JsonSuccess(null, "Delete Succses.");
+        //}
+
+        public JsonResult JsonSuccess(dynamic data, string message = "")
+        {
+            return Json(new
+            {
+                IsSuccess = true,
+                Message = message,
+                Data = data
+            }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult JsonError(string message)
+        {
+            return Json(new
+            {
+                IsSuccess = false,
+                Message = message
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TodoList.Web/Models/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TodoApp.Web.Controllers
+{
+    public class Todo
+    {
+        public Todo()
+        {
+            Created = DateTime.Now;
+            Id = Guid.NewGuid();
+        }
+
+        public Guid? Id { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Modified { get; set; }
+        
+    }
+}
